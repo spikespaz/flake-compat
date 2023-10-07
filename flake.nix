0088efaff632b01f6cec9387659832fc9b4474a5
Jacob Birkett <spikespaz@outlook.com>
2023-10-07 03:23:55 -0700

    flake: add lib output

--- flake.nix
+++ flake.nix
@@ -2,5 +2,9 @@
   description = "Allow flakes to be used with Nix < 2.4";
 
   outputs = { self }: {
+    lib = {
+      lib = (self.overlay null null).fc;
+      overlay = import ./lib.nix;
+    };
   };
 }
